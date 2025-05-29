3e8315cf885fa3fcd102d6530a94187066863705
Girishmasade <girishmasade@gmail.com>
2025-05-29 21:37:20 +0530

    Add upload transaction file mutation to transaction API slice
    Signed-off-by: Girishmasade <girishmasade@gmail.com>

--- client/src/Redux/app/transactionApiSlice.js
+++ client/src/Redux/app/transactionApiSlice.js
@@ -1,41 +1,55 @@
 import { apiSlice } from "../api/apiSlice";
-const TRANSACTION_URI = 'transaction'
+const TRANSACTION_URI = "transaction";
 
 export const transactionApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        addTransaction: builder.mutation({
-            query: (data) => ({
-                url: `${TRANSACTION_URI}/add-transaction`,
-                method: 'POST',
-                body: data,
-                credentials: 'include'
-            })
-        }), 
+  endpoints: (builder) => ({
+    addTransaction: builder.mutation({
+      query: (data) => ({
+        url: `${TRANSACTION_URI}/add-transaction`,
+        method: "POST",
+        body: data,
+        credentials: "include",
+      }),
+    }),
 
-        getTransactions: builder.query({
-            query: () => ({
-                url: `${TRANSACTION_URI}/get-transaction`,
-                method: 'GET',
-                credentials: 'include'
-            })
-        }),
+    getTransactions: builder.query({
+      query: () => ({
+        url: `${TRANSACTION_URI}/get-transaction`,
+        method: "GET",
+        credentials: "include",
+      }),
+    }),
 
-        updateTransactions: builder.mutation({
-            query: ({id, updateData}) => ({
-                url: `${TRANSACTION_URI}/update-transaction/${id}`,
-                method: 'PUT',
-                body: {updateData},
-                credentials: 'include'
-            })
-        }),
-        deleteTransactions: builder.mutation({
-            query: (id) => ({
-                url: `${TRANSACTION_URI}/delete-transaction/${id}`,
-                method: 'DELETE',
-                credentials: 'include'
-            })
-        })
-    })
-})
+    updateTransactions: builder.mutation({
+      query: ({ id, updateData }) => ({
+        url: `${TRANSACTION_URI}/update-transaction/${id}`,
+        method: "PUT",
+        body: { updateData },
+        credentials: "include",
+      }),
+    }),
+    deleteTransactions: builder.mutation({
+      query: (id) => ({
+        url: `${TRANSACTION_URI}/delete-transaction/${id}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+    }),
+    uploadTransacationFile: builder.mutation({
+      query: (body) => ({
+        url: `${TRANSACTION_URI}/upload-transaction`,
+        method: "POST",
+        body,
+        credentials: "include",
+      }),
+    }),
+  }),
+});
 
-export const {useAddTransactionMutation, useGetTransactionsQuery, useUpdateTransactionsMutation, useDeleteTransactionsMutation} = transactionApiSlice
+export const {
+  useAddTransactionMutation,
+  useGetTransactionsQuery,
+  useUpdateTransactionsMutation,
+  useDeleteTransactionsMutation,
+  useUploadTransacationFileMutation
+} = transactionApiSlice;
