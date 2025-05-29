ad66a9be0f7597eab9cb95e30f3e3b358340f1a4
Girishmasade <girishmasade@gmail.com>
2025-05-29 21:22:08 +0530

    Implement transaction upload functionality and update routes for Excel file handling
    Signed-off-by: Girishmasade <girishmasade@gmail.com>

--- server/routers/transaction.route.js
+++ server/routers/transaction.route.js
@@ -1,6 +1,7 @@
 import {Router} from 'express'
-import { addTransaction, deleteTransaction, getTransaction, updateTransactions } from '../controllers/transaction.controller.js'
+import { addTransaction, deleteTransaction, getTransaction, updateTransactions, uploadExcelTransaction } from '../controllers/transaction.controller.js'
 import { protectRoute } from '../middleware/protecteRoute.middleware.js'
+import { uploadXlsx } from '../middleware/multer.middleware.js'
 
 const transactionRoute = Router()
 
@@ -8,5 +9,6 @@ transactionRoute.post('/add-transaction', protectRoute, addTransaction)
 transactionRoute.get('/get-transaction', protectRoute, getTransaction)
 transactionRoute.put('/update-transaction/:id', protectRoute, updateTransactions)
 transactionRoute.delete('/delete-transaction/:id', protectRoute, deleteTransaction)
+transactionRoute.post('/upload-transaction', protectRoute, uploadXlsx.single('file'), uploadExcelTransaction)
 
 export default transactionRoute
